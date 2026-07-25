db479a5b6418adbf8cfdbbc5cacd6d2591fe573d
Sahid Reza <sahidreza976@gmail.com>
2026-07-25 13:47:05 +0530

    Add full code

--- src/main/java/org/example/Majority_Element.java
+++ src/main/java/org/example/Majority_Element.java
@@ -1,7 +1,27 @@
 package org.example;
 
-public class Majority_Element {
-    public static void main(String[] args) {
+import java.util.HashMap;
 
+public class Majority_Element {
+    public static void majorityElement(int nums[]){
+        HashMap<Integer, Integer>map = new HashMap<>();
+        int n = nums.length;
+        for(int i = 0;i<nums.length;i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i] )+ 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+        for(int key : map.keySet()){
+            if(map.get(key)> n/3) {
+                System.out.println(key);
+            }
+        }
+
+    }
+    public static void main(String[] args) {
+        int num[] = {1,3,2,5,1,3,1,5,1};
+        majorityElement(num);
     }
 }
